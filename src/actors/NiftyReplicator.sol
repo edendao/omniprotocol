b0bf22f230c84e5bdb17a2c7af96364e58747fa0
Kash Nouroozi <knouroozi@gmail.com>
2022-04-04 17:16:56 +0200

    Nifty replicator works :)

--- src/actors/NiftyReplicator.sol
+++ src/actors/NiftyReplicator.sol
@@ -7,18 +7,50 @@ import { Passport } from "@protocol/Passport.sol";
 import { Omnichain } from "@protocol/mixins/Omnichain.sol";
 import { Authenticated } from "@protocol/mixins/Authenticated.sol";
 
-abstract contract NiftyReplicator is Omnichain, Authenticated {
+contract NiftyReplicator is Omnichain, Authenticated {
   Passport public immutable passport;
 
   mapping(uint256 => uint16) public tokenChain;
   mapping(uint256 => address) public tokenAddress;
   mapping(uint256 => uint256) public tokenId;
 
-  constructor(address _authority, address _passport) Authenticated(_authority) {
+  uint16 public immutable primaryChainId;
+
+  constructor(
+    address _authority,
+    address _lzEndpoint,
+    address _passport,
+    uint16 _primaryChainId
+  ) Omnichain(_lzEndpoint) Authenticated(_authority) {
+    primaryChainId = _primaryChainId;
     passport = Passport(_passport);
   }
 
-  function lock(address collection, uint256 id) external {
+  modifier onlyPrimaryChain() {
+    require(primaryChainId == block.chainid, "Passport: Only on primary chain");
+    _;
+  }
+
+  function tokenOf(address user)
+    public
+    view
+    returns (
+      uint16,
+      address,
+      uint256,
+      string memory
+    )
+  {
+    uint256 id = passport.idOf(user);
+    return (
+      tokenChain[id],
+      tokenAddress[id],
+      tokenId[id],
+      passport.tokenURI(id)
+    );
+  }
+
+  function lock(address collection, uint256 id) external onlyPrimaryChain {
     uint256 passportId = passport.idOf(msg.sender);
 
     ERC721 nifty = ERC721(collection);
@@ -26,39 +58,41 @@ abstract contract NiftyReplicator is Omnichain, Authenticated {
 
     bytes memory uri = bytes(nifty.tokenURI(id));
 
-    if (passport.balanceOf(msg.sender) == 0) {
-      passport.mintTo(msg.sender);
-    } else if (tokenAddress[passportId] != address(0)) {
-      ERC721(tokenAddress[passportId]).safeTransferFrom(
-        address(this),
-        msg.sender,
-        tokenId[passportId]
-      );
-    }
-
+    passport.ensureMintedTo(msg.sender);
     passport.setTokenURI(passportId, uri);
+
+    if (tokenAddress[passportId] != address(0)) {
+      _dangerouslyReleaseUnderlyingNiftyOf(passportId);
+    }
 
     tokenChain[passportId] = uint16(block.chainid);
     tokenAddress[passportId] = collection;
     tokenId[passportId] = id;
   }
 
-  function release() external {
+  function release() external onlyPrimaryChain {
     uint256 passportId = passport.idOf(msg.sender);
+    _dangerouslyReleaseUnderlyingNiftyOf(passportId);
+    passport.setTokenURI(passportId, "");
+  }
+
+  function _dangerouslyReleaseUnderlyingNiftyOf(uint256 passportId)
+    private
+    onlyPrimaryChain
+  {
     ERC721(tokenAddress[passportId]).safeTransferFrom(
       address(this),
       msg.sender,
       tokenId[passportId]
     );
-    passport.setTokenURI(passportId, "");
   }
 
-  function sync(
+  function lzSync(
     uint16 toChainId,
     address owner,
     address zroPaymentAddress,
     bytes calldata adapterParams
-  ) external payable {
+  ) external payable onlyPrimaryChain {
     uint256 id = passport.idOf(owner);
 
     lzEndpoint.send{ value: msg.value }(
